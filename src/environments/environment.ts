2689deec601e0e2cdd75fa55970531ccbac7b5b3
M4TEO <74993888+MateoCueva03@users.noreply.github.com>
2022-11-27 21:28:06 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyB9eWc2c6Bso3d1bM8cyKMPoWr_iJstpfk",
-  authDomain: "comet-4bf14.firebaseapp.com",
-  projectId: "comet-4bf14",
-  storageBucket: "comet-4bf14.appspot.com",
-  messagingSenderId: "556729773325",
-  appId: "1:556729773325:web:999d010178b08aca919444",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
   }};
 
 //
